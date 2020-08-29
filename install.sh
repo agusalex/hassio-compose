e4803f9a220a5b8068a0b3224a9475de65a56e3e
Agustin Alexander <aalexander@campus.ungs.edu.ar>
2020-08-29 14:38:13 -0300

    update

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 sudo apt update -y;
 sudo apt-get install -y libffi-dev libssl-dev python3 python3-pip;
 curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh;
+echo "SUPERVISOR_SHARE=/home/$USER/hassio-config" > .env;
 sudo usermod -a -G docker ${USER};
 sudo su - ${USER};
 sudo pip3 -v install docker-compose;
-echo "SUPERVISOR_SHARE=/home/$USER/hassio-config" > .env;
 docker-compose up -d;
