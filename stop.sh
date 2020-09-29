e226bff89790331c2fbfeaca30da921cd9cc1ff9
Agustin Alexander <agustin.c.alexander@gmail.com>
2020-09-29 19:48:57 -0300

    updated script

--- stop.sh
+++ stop.sh
@@ -1,3 +1,3 @@
-$ docker-compose down ;
-$ docker ps | grep "homeassistant" | cut -f 1 -d ' ' | xargs docker stop ;
-$ docker ps | grep "hassio" | cut -f 1 -d ' ' | xargs docker stop ;
+docker-compose down ;
+docker ps | grep "homeassistant" | cut -f 1 -d ' ' | xargs docker stop ;
+docker ps | grep "hassio" | cut -f 1 -d ' ' | xargs docker stop ;
