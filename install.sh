13f15ed3f6f924d805975b02fbb488d36f64db40
Agustin Alexander <agustin.c.alexander@gmail.com>
2020-10-08 15:31:41 -0300

    update install

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+#!/bin/sh
+sudo perl -pi -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen; #Fix locale error
+sudo apt update -y;
+sudo apt-get install -y libffi-dev libssl-dev python3 python3-pip git;
 curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh;
 sudo usermod -a -G docker ${USER};
 sudo su - ${USER};
